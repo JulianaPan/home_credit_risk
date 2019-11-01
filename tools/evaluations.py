ca4d393cfbb34b3db2bb64f069c25492f98fd79b
Genrong Zheng <john.zhenggenrong@outlook.com>
2019-11-01 10:27:19 +0800

    add model

--- tools/evaluations.py
+++ tools/evaluations.py
@@ -9,9 +9,9 @@ def plot_ROC(y_test, y_pred, title):
     roc_auc = auc(false_positive_rate, recall)
     fig = plt.gcf()
     plt.title(title, fontsize=15)
-    ks_index = np.argmax(recall-false_positive_rate)
-    plt.plot([false_positive_rate[ks_index],false_positive_rate[ks_index]],
-             [false_positive_rate[ks_index], recall[ks_index]],'r')
+    #ks_index = np.argmax(recall-false_positive_rate)
+    #plt.plot([false_positive_rate[ks_index],false_positive_rate[ks_index]],
+     #        [false_positive_rate[ks_index], recall[ks_index]],'r')
     plt.plot(false_positive_rate, recall, 'b', label='AUC = %0.2f' % roc_auc)  # AUC值
     plt.legend(loc='lower right')
     plt.plot([0, 1], [0, 1], 'r--')
@@ -20,6 +20,7 @@ def plot_ROC(y_test, y_pred, title):
     plt.ylabel('Recall', fontsize=15)
     plt.xlabel('Fall-out', fontsize=15)
     fig.set_size_inches(10, 6)
+    plt.savefig('./figures/'+title+'.png')
     plt.show()
     
     print('AUC: ',roc_auc)
